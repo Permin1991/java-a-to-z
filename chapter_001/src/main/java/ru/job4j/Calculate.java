01f4d6544f06e2b668c1640cd640a822ac8af9cf
Permin1991 <danya1991@list.ru>
2017-03-12 21:27:41 +0300

    task 4086

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,18 @@
 package ru.job4j;
 
+/**
+*Calculate
+*@author permin daniil
+*@since 12.03.2017
+*/ 
+
 public class Calculate {
+
+/**
+*Вывод строки в консоль
+*@param arg-arg
+*/
+
 public static void main(String[] args) {
 System.out.println("Hello world.");
 }
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* //Пакет для калькулятора
+*
+* @author Permin Daniil (mailto:danya1991@list.ru)
+* @version $Id$
+* @since 12.03.17
+*/
+package ru.job4j;
